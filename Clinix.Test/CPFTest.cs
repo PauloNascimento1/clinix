57f8ea9914f6f5486db0fd46a633fbc87c98a271
PauloNascimento1 <illupaulo7@gmail.com>
2026-02-06 13:41:04 -0300

    feat: incluindo testes de entidade para cpf

---
+++ Clinix.Test/CPFTest.cs
@@ -0,0 +1,24 @@
+﻿namespace Clinix.Test;
+
+public class CPFTest
+{
+    [Fact]
+    public void Constructor_WhenCpfIsNull_ShouldThrowArgumentNullException()
+    {
+        string value = "";
+
+        Assert.Throws<ArgumentException>(() =>
+            new CPF(value)
+        );
+    }
+
+    [Fact]
+    public void Constructor_WhenCpfInvalidLength_ShouldThrowArgumentNullException()
+    {
+        string value = "1234567890";
+
+        Assert.Throws<ArgumentException>(() =>
+            new CPF(value)
+        );
+    }
+}
